1fa07052e5265f6886314d85560333d4266a9673
Elina Osmala <n0osel00@students.oamk.fi>
2021-03-15 17:55:38 +0200

    Bare base done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Home from './Home';
+import Nav from './Nav';
+import Products from './Products'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <div className="container">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/products" component={Products} />
+        </Switch>
+      </div>
+      <Footer />
+    </>
   );
 }
 
