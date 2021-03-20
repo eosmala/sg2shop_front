d4d19f3d3305cb09f41b516c81926ce06a26a8ca
sHautala <sjhautala98@gmail.com>
2021-03-20 14:26:39 +0200

    add navbar + jquery to index.html

--- src/Nav.js
+++ src/Nav.js
@@ -4,8 +4,22 @@ import {Link} from 'react-router-dom';
 export default function Nav() {
     return (
         <div>
-            <Link to="/Home">Home</Link>
-            <Link to="/Products">Products</Link>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ paddingLeft: '12px' }}>
+                <a className="navbar-brand"><Link to="/" style={{ textDecoration: 'none', color: 'black' }}>Elukoille rojuja</Link></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link"><Link to="/" style={{ textDecoration: 'none', color: 'black' }}>Home</Link></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link"><Link to="/Products" style={{ textDecoration: 'none', color: 'black' }}>Products</Link></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
