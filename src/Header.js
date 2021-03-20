21e4f217a82b63812de2b7b256b3efc4da1a477c
Elina Osmala <n0osel00@students.oamk.fi>
2021-03-20 13:50:50 +0200

    Update header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import banner from './img/header.png';
 
 export default function Header() {
     return (
-        <div className="">
-            <h2>Lemmikkitarvikekauppa</h2>
+        <div className="col-8 col-md-5">
+            <img className="img-fluid mw-100 "
+            src = {banner} alt="header"
+            />
         </div>
     )
 }
