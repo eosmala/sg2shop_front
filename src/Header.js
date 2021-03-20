03096fe822844089b41fc2ef1f5ddd6fbb7f1ea6
Elina Osmala <n0osel00@students.oamk.fi>
2021-03-20 13:55:38 +0200

    Update header smaller

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import banner from './img/header.png';
 
 export default function Header() {
     return (
-        <div className="col-8 col-md-5">
+        <div className="col-7 col-md-3">
             <img className="img-fluid mw-100 "
             src = {banner} alt="header"
             />
