cf51cc564c2a6eb7376c809b097dbd6ed5c72928
Elina Osmala <n0osel00@students.oamk.fi>
2021-03-20 14:13:46 +0200

    Update Footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className="footer fixed-bottom m-2">
+        <div className="footer m-2">
             <p className="fw-light fst-italic">Elukoille rojuja Oy</p>
         </div>
     )
