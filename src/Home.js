7bb33d301109ae69e2a0582bc50999be930cb6ca
TonKeee <74178541+TonKeee@users.noreply.github.com>
2021-03-20 13:59:50 +0200

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,91 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            <p>Home</p>
+    <div>
+        <section class="masthead text-white text-center bg-dark">
+        <div class="overlay"></div>
+        <div class="container">
+        <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-5">section 1</h1>
+          <p>Bottom text</p>
         </div>
+            <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+        </div>
+        </div>
+        </div>
+        </section>
+        <section class="features-icons bg-light text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-screen-desktop m-auto text-primary"></i>
+            </div>
+            <h3>Tuote 1</h3>
+            <p class="lead mb-0">Tuote 1 kuvaus</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-layers m-auto text-primary"></i>
+            </div>
+            <h3>Tuote 2</h3>
+            <p class="lead mb-0">Tuote 2 kuvaus</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-check m-auto text-primary"></i>
+            </div>
+            <h3>Tuote 3</h3>
+            <p class="lead mb-0">Tuote 3 kuvaus</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+  <div class="row">
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h4 class="card-title">Tuoteryhmä 1</h4>
+            <p class="card-text">placeholder</p>
+          </div>
+          <div class="card-footer">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h4 class="card-title">Tuoteryhmä 2</h4>
+            <p class="card-text">placeholder</p>
+          </div>
+          <div class="card-footer">
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h4 class="card-title">Tuoteryhmä 3</h4>
+            <p class="card-text">placeholder</p>
+          </div>
+          <div class="card-footer">
+              <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+    </div>
     )
 }
