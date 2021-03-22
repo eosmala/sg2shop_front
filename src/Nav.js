b5ddb35f0ff9b7d53c4194039136b5107b780c23
sHautala <sjhautala98@gmail.com>
2021-03-22 13:04:06 +0200

    change navbar width

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 export default function Nav() {
     return (
-        <div>
+        <div class="container">
             <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ paddingLeft: '12px' }}>
                 <Link to="/" className="navbar-brand text-decoration-none text-dark">Elukoille rojuja</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
