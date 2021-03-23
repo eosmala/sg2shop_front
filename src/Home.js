220f4a7b652db3434b818822e04c0a67246d16d8
Tuomas <n9katu00@students.oamk.fi>
2021-03-23 16:10:34 +0200

    Font style changes and placeholder carousel idea.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import add1 from './img/mainos1.jpg'
-
+import temp1 from './img/placeholder.jpg'
+import temp2 from './img/placeholder2.jpg'
+import temp3 from './img/placeholder3.jpg'
 export default function Home() {
     return (
         <div>
@@ -16,7 +18,7 @@ export default function Home() {
                                 />
                             </div>
                         </div>
-                        <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                        <div className="col-md-10 col-lg-8 col-xl-7 mx-auto pb-4">
                         </div>
                     </div>
                 </div>
@@ -59,7 +61,33 @@ export default function Home() {
                     <div className="col-md-4 mb-5">
                         <div className="card h-100">
                             <div className="card-body">
-                                <h4 className="card-title">Tuoteryhmä 1</h4>
+                                <h4 className="card-title">Ruoka tuotteita</h4>
+                                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-target="#carouselExampleIndicators" data-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-target="#carouselExampleIndicators" data-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-target="#carouselExampleIndicators" data-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner"> 
+    <div class="carousel-item active">
+      <img src={temp1} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={temp2} class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src={temp3} class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators"  data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators"  data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div> 
                                 <p className="card-text">placeholder</p>
                             </div>
                             <div className="card-footer">
@@ -69,7 +97,7 @@ export default function Home() {
                     <div className="col-md-4 mb-5">
                         <div className="card h-100">
                             <div className="card-body">
-                                <h4 className="card-title">Tuoteryhmä 2</h4>
+                                <h4 className="card-title">Suosituimmat tuotteet</h4>
                                 <p className="card-text">placeholder</p>
                             </div>
                             <div className="card-footer">
@@ -79,7 +107,7 @@ export default function Home() {
                     <div className="col-md-4 mb-5">
                         <div className="card h-100">
                             <div className="card-body">
-                                <h4 className="card-title">Tuoteryhmä 3</h4>
+                                <h4 className="card-title">Leluja</h4>
                                 <p className="card-text">placeholder</p>
                             </div>
                             <div className="card-footer">
