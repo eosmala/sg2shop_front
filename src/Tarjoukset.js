775457d5235238d5fe57e2e93195190409b75865
sHautala <sjhautala98@gmail.com>
2021-04-06 13:23:08 +0300

    merger

--- src/Tarjoukset.js
+++ src/Tarjoukset.js
@@ -1,12 +1,12 @@
-import React,  { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import placeholder from './img/logo.png';
 
-export default function Tarjoukset({url, category, addToCart}) {
+export default function Tarjoukset({ url, category, addToCart }) {
     const [products, setProducts] = useState([]);
 
     const [cart, setCart] = useState([]);
 
-    useEffect(async() => {
+    useEffect(async () => {
         if (category !== null) {
             let address = '';
             if (category !== null) {
@@ -23,27 +23,27 @@ export default function Tarjoukset({url, category, addToCart}) {
             } catch (error) {
                 alert(error);
             }
-            }
+        }
     }, [category])
 
     return (
         <div className="container">
             <p>Tarjoustuotteet</p>
             <h3>Products for {category?.name}</h3>
-                        {products.map(product => (
-                             <div className="card p-2 m-2" style={{width: "18rem", display: "inline-block", border: "1px solid black"}}>
-                            <img className="card-img-top" src={placeholder} alt="Card image cap"/>
-                            <div className="card-body">
-                            <h5 className="card-title" key={product.id}>
-                                <hr></hr>
-                                <p>{product.name}</p>
-                            </h5>
-                            <button type="button" className="btn btn-primary">Lisää ostoskoriin <i className="fa fa-shopping-cart" aria-hidden="true"></i></button>
-                            </div>
-                            </div>
-                        ))}
+            {products.map(product => (
+                <div className="card p-2 m-2" style={{ width: "18rem", display: "inline-block", border: "1px solid black" }}>
+                    <img className="card-img-top" src={placeholder} alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title" key={product.id}>
+                            <hr></hr>
+                            <p>{product.name}</p>
+                        </h5>
+                        <button type="button" className="btn btn-primary">Lisää ostoskoriin <i className="fa fa-shopping-cart" aria-hidden="true"></i></button>
+                    </div>
+                </div>
+            ))}
         </div>
-            
+
 
     )
 }
