54ac24eb22fe997893bebcc50edbbccd27744f0a
sHautala <sjhautala98@gmail.com>
2021-04-06 13:01:50 +0300

    add base for Tarjoukset -page

--- src/Tarjoukset.js
+++ src/Tarjoukset.js
@@ -1,5 +1,5 @@
 import React,  { useState, useEffect } from 'react'
-
+import placeholder from './img/logo.png';
 
 export default function Tarjoukset({url, category, addToCart}) {
     const [products, setProducts] = useState([]);
@@ -31,11 +31,19 @@ export default function Tarjoukset({url, category, addToCart}) {
             <p>Tarjoustuotteet</p>
             <h3>Products for {category?.name}</h3>
                         {products.map(product => (
-                            <div key={product.id}>
+                             <div className="card p-2 m-2" style={{width: "18rem", display: "inline-block", border: "1px solid black"}}>
+                            <img className="card-img-top" src={placeholder} alt="Card image cap"/>
+                            <div className="card-body">
+                            <h5 className="card-title" key={product.id}>
+                                <hr></hr>
                                 <p>{product.name}</p>
+                            </h5>
+                            <button type="button" className="btn btn-primary">Lisää ostoskoriin <i className="fa fa-shopping-cart" aria-hidden="true"></i></button>
+                            </div>
                             </div>
                         ))}
-                 
         </div>
+            
+
     )
 }
