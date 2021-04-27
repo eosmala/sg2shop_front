d5ec2af2a726cd1f28cccdf8dbb3322387bb42a1
Elina Osmala <n0osel00@students.oamk.fi>
2021-04-27 13:56:55 +0300

    add product complete, delete product button working

--- src/admin/Addproduct.js
+++ src/admin/Addproduct.js
@@ -1,16 +1,60 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Addproduct ({ URL }) {
+export default function Addproduct ({ url }) {
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const [price, setPrice] = useState('')
   const [stock, setStock] = useState('')
-  const [category, setCategory] = useState('')
+  const [category, setCategory] = useState(1)
+
+  function reset() {
+    setCategory(1)
+    setName('')
+    setPrice('')
+    setStock('')
+    setDescription('')
+  }
+  function save(e) {
+    e.preventDefault()
+    let status = 0
+    fetch(url + 'admin/addproduct.php', {
+      method: 'POST',
+      headers: {
+        Accept: 'application(json)',
+        'Content-type': 'application(json)'
+      },
+      body: JSON.stringify({
+        product_name: name,
+        price: price,
+        stock_amount: stock,
+        category_id: category,
+        description: description,
+      })
+    })
+      .then(res => {
+        status = parseInt(res.status)
+        return res.json()
+      })
+      .then(
+        res => {
+          if (status === 200) {
+           alert("lisäys onnistui")
+           reset();
+          } else {
+            alert(res.error)
+          }
+        },
+        error => {
+          alert(error)
+        }
+      )
+  }
 
   return (
-    <div className='row'>
+    <div className='row my-3'>
       <h4>Lisää tuote</h4>
-      <form onSubmit=''>
+      <form onSubmit={save}>
         <div className='form-group row'>
           <label for='nimi' className='col-sm-2 col-form-label'>
             Tuotenimi
@@ -26,10 +70,11 @@ export default function Addproduct ({ URL }) {
           </div>
         </div>
         <div className='form-group row'>
-          <label className='col-sm-2 col-form-label'>Kuvaus</label>
+          <label for="description" className='col-sm-2 col-form-label'>Kuvaus</label>
           <div className='col-sm-10'>
             <input
               type='text'
+              id="description"
               className='form-control'
               value={description}
               onChange={e => setDescription(e.target.value)}
@@ -37,10 +82,11 @@ export default function Addproduct ({ URL }) {
           </div>
         </div>
         <div className='form-group row'>
-          <label className='col-sm-2 col-form-label'>Hinta</label>
+          <label for="price" className='col-sm-2 col-form-label'>Hinta</label>
           <div className='col-sm-10'>
             <input
               type='number'
+              id='price'
               className='form-control'
               value={price}
               onChange={e => setPrice(e.target.value)}
@@ -48,10 +94,11 @@ export default function Addproduct ({ URL }) {
           </div>
         </div>
         <div className='form-group row'>
-          <label className='col-sm-2 col-form-label'>Varastomäärä</label>
+          <label for="stock" className='col-sm-2 col-form-label'>Varastomäärä</label>
           <div className='col-sm-10'>
             <input
               type='number'
+              id="stock"
               className='form-control'
               value={stock}
               onChange={e => setStock(e.target.value)}
@@ -82,16 +129,16 @@ export default function Addproduct ({ URL }) {
             <input type='file' class='form-control-file' id='tiedosto' />
           </div>
         </div>
-        <div className="text-right">
- 
-            <button type='submit' className='btn'>
-              Tallenna
-            </button>
-
-            <button type='submit' className='btn'>
-              tyhjennä
-            </button>
-
+        <div className='form-group text-right'>
+          <Link className='btn mx-2' to='/admin'>
+            Takaisin
+          </Link>
+          <button type='reset' className='btn mx-2' onClick={reset}>
+            Tyhjennä
+          </button>
+          <button type='submit' className='btn mx-2'>
+            Tallenna
+          </button>
         </div>
       </form>
     </div>
--- src/admin/Aproducts.js
+++ src/admin/Aproducts.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-
+import { Link } from 'react-router-dom'
 
 export default function Aproducts ({ url }) {
-  const [products, setProducts] = useState([]);
-  const [productname, setProductname] = useState('');
-  const [productId, setProductId] = useState('');
+  const [products, setProducts] = useState([])
+  const [productname, setProductname] = useState('')
+ // const [productId, setProductId] = useState('')
+
   useEffect(() => {
     async function prod () {
       try {
@@ -24,55 +25,72 @@ export default function Aproducts ({ url }) {
     prod()
   }, [url])
 
+  function remove(id){
+    console.log(id)
+    
+  }
+
   return (
-    <div className='container' id='category-page'>
-      <h3>Tuotteet</h3>
-      {products.map(product => (
-        <div key={product.product_id} className='p-2 m-2'>
-          <p>Tuotenumero: {product.product_id} </p>
-          <p>Tuotenimi: {product.product_name}</p>
-          <p>Hinta: {product.price} € </p>
-          <p>Varastossa: {product.stock_amount} kpl</p>
-          <p>Kategoria: {product.category_name}</p>
-          <p>
-            <button
-              className='btn'
-              type='button'
-              data-toggle='collapse'
-              data-target={`#collapse${product.product_id}`}
-              aria-expanded='false'
-              aria-controls='collapse'
-              onClick={'setProductname({product.product_name})'}
-            >
-              muokkaa
-            </button>
-          </p>
-          <div className='collapse' id={`collapse${product.product_id}`}>
-            <form action=''>
-              <label for='product_name'>Tuotenimi</label>
-              <input
-                name='product_name'
-                type='text'
-                value={productname}
-                onChange={e => setProductname(e.target.value)}
-              />
+    <div className='container my-3'>
+      <div className='text-center'>
+        <h3>Tuotteet</h3>
+        <Link className='btn mx-2' to='/admin'>
+          Takaisin
+        </Link>
+        <Link className='btn mx-2' to='/admin/addproduct'>
+          Lisää tuote
+        </Link>
+      </div>
+      <hr></hr>
+      <div className='row'>
+        {products.map(product => (
+          <div key={product.product_id} className='p-2 m-2 col-10 col-md-3'>
+            <p>Tuotenumero: {product.product_id} </p>
+            <p>Tuotenimi: {product.product_name}</p>
+            <p>Hinta: {product.price} € </p>
+            <p>Varastossa: {product.stock_amount} kpl</p>
+            <p>Kategoria: {product.category_name}</p>
+            <p>
+              <button
+                className='btn m-2'
+                type='button'
+                // data-toggle='collapse'
+                // data-target={`#collapse${product.product_id}`}
+                // aria-expanded='false'
+                // aria-controls='collapse'
+                // onClick={'setProductname({product.product_name})'}
+              >
+                muokkaa
+              </button> 
+              <button className='btn m-2' 
+              value={`${product.product_id}`} onClick={e => remove(e.target.value)} 
+              >
+                poista
+              </button>
+            </p>
+            {/* <div className='collapse' id={`collapse${product.product_id}`}>
+              <form action=''>
+                <label for='product_name'>Tuotenimi</label>
+                <input
+                  name='product_name'
+                  type='text'
+                  value={productname}
+                  onChange={e => setProductname(e.target.value)}
+                />
                 <label for='price'>Hinta</label>
-              <input
-                name='price'
-                type='number'
-                value={product.price}
-              />
+                <input name='price' type='number' value={product.price} />
                 <label for='stock_amount'>Varastossa</label>
-              <input
-                name='stock_amount'
-                type='number'
-                value={product.stock_amount}
-              />
-            </form>
+                <input
+                  name='stock_amount'
+                  type='number'
+                  value={product.stock_amount}
+                />
+              </form>
+            </div> */}
+            <hr></hr>
           </div>
-          <hr></hr>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
