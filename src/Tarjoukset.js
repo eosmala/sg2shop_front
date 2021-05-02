67acf954b86482b07a1478a10f6c997de609a5a2
Elina Osmala <n0osel00@students.oamk.fi>
2021-05-02 11:40:39 +0300

    key from id to product_id

--- src/Tarjoukset.js
+++ src/Tarjoukset.js
@@ -32,7 +32,7 @@ export default function Tarjoukset({ url, category, addToCart }) {
             <h3 className="mt-1">{category?.name}</h3>
             <h5 id="category-info">Tarjoustuotteet</h5>
             {products.map(product => (
-                <div key={product.id} className="card p-2 m-2" style={{ width: "18rem", display: "inline-block", border: "1px solid black" }}>
+                <div key={product.product_id} className="card p-2 m-2" style={{ width: "18rem", display: "inline-block", border: "1px solid black" }}>
                     <Link to={{pathname: "/product/" + product.product_id}}><img className="card-img-top" src={url + "products/img/" + product.image} alt={product.product_name} /></Link>
                     <div className="card-body">
                         <div className="card-title" key={product.product_id}>
